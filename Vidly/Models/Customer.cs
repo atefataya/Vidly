5202634364798d0a95c06845fbb9f04f38694eab
Atef Ataya <atef.ataya@yahoo.com>
2017-01-13 19:03:44 +0400

    Membership type
    Add property IsSubscribed, MemebershipType, MemeberShiptTypeId to
    Customer.cs class in the Model
    Add a new class called memebershiptype with properties: Id, SignUpFee,
    DurationInMonths, DiscountRate
    Then in the package manager add-migration and update-database

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,11 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        public byte MememberShipTypeId { get; set; }
+
     }
 }
